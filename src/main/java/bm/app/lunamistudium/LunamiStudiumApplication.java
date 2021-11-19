cea3ba37dbbd2244e934a1639e0057bb51799b2a
Bartosz76 <mierzwinski.b@gmail.com>
2021-11-19 23:05:42 +0100

    The initial commit.

---
+++ src/main/java/bm/app/lunamistudium/LunamiStudiumApplication.java
@@ -0,0 +1,13 @@
+package bm.app.lunamistudium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunamiStudiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunamiStudiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bm/app/lunamistudium/LunamiStudiumApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.app.lunamistudium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunamiStudiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
