b24a57cc8bf5ff7aade353b4bf1e6f72a66d79ba
bmierzwinski <bartosz.mierzwinski@cedc.com>
2021-12-01 23:08:44 +0100

    Functionalities converting binary numbers into hexadecimal ones and vice versa were added to the controller and their endpoints are available via Swagger.

--- src/main/java/bm/app/lunamistudium/technology/converter/web/ConverterController.java
+++ src/main/java/bm/app/lunamistudium/technology/converter/web/ConverterController.java
@@ -18,14 +18,26 @@ public class ConverterController {
     }
 
     @PostMapping("/decToBin/{decimal}")
-    @ApiOperation(value = "Converts a decimal number into a binary one.", notes = "Provide a decimal number")
+    @ApiOperation(value = "Converts a decimal number into a binary one.", notes = "Provide a decimal number.")
     public int getDecimalToBinary(@PathVariable int decimal) {
         return converterUseCase.convertDecimalToBinary(decimal);
     }
 
     @PostMapping("/binToDec/{binary}")
-    @ApiOperation(value = "Converts a binary number into a decimal one.", notes = "Provide a binary number")
+    @ApiOperation(value = "Converts a binary number into a decimal one.", notes = "Provide a binary number.")
     public int getBinaryToDecimal(@PathVariable int binary) {
         return converterUseCase.convertBinaryToDecimal(binary);
+    }
+
+    @PostMapping("/binToHex/{binary}")
+    @ApiOperation(value = "Converts a binary number into a hexadecimal one.", notes = "Provide a binary number.")
+    public String getBinaryToHexadecimal(@PathVariable String binary) {
+        return converterUseCase.convertBinaryToHexadecimal(binary);
+    }
+
+    @PostMapping("/hexToBin/{hexadecimal}")
+    @ApiOperation(value = "Converts a hexadecimal number into a binary one.", notes = "Provide a hexadecimal number.")
+    public String getConvertHexadecimalToBinary(@PathVariable String hexadecimal) {
+        return converterUseCase.convertHexadecimalToBinary(hexadecimal);
     }
 }
